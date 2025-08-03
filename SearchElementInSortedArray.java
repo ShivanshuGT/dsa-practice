f3224b606330ba2e9454b331eb511a31a26d2ffa
ShivanshuGT <shivanshumdnr@gmail.com>
2025-08-03 21:52:14 +0530

    search element in rotated sorted array part2

--- SearchElementInSortedArray.java
+++ SearchElementInSortedArray.java
@@ -36,6 +36,46 @@ public class SearchElementInSortedArray {
         // TC -> O(logn)
         // SC -> O(1)
     }
+
+    private static boolean checkIfElementIsPresentIfArrayHasDuplicates(int arr[], int target){
+        int n = arr.length;
+        int beg = 0;
+        int end = n-1;
+        while (beg <= end) {
+            int mid = (beg + end) / 2;
+            if(arr[mid] == target){
+                return true;
+            }
+
+            if(arr[mid] == arr[beg] && arr[mid] == arr[end]){
+                beg = beg + 1;
+                end = end - 1;
+                continue;
+            }
+
+            if(arr[mid] <= arr[end]){
+                if(arr[mid] <= target && target <= arr[end]){
+                    beg = mid + 1;
+                }
+                else{
+                    end = mid - 1;
+                }
+            }else{
+                if(arr[beg] <= target && target <= arr[mid]){
+                    end = mid - 1;
+                }else{
+                    beg = mid + 1;
+                }
+            }
+        }
+
+        return false;
+
+        // TC -> O(logn) -> best and average case
+        // TC -> O(n/2) -> worst case, when there are huge number of duplicates in the array
+        // like arr[] = [3,3,3,3,3,3,3] and target = 1
+        // SC -> O(1)
+    }
  
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -46,7 +86,14 @@ public class SearchElementInSortedArray {
             arr[i] = sc.nextInt();
         }
 
-        System.out.println("Element found at index " + findElement(arr, target));
+        // System.out.println("Element found at index " + findElement(arr, target));
+        boolean isPresent = checkIfElementIsPresentIfArrayHasDuplicates(arr, target);
+        if(isPresent){
+            System.out.println("Element is present in the array.");
+        }else{
+            
+            System.out.println("Element is not present in the array.");
+        }
         sc.close();
     }
 }
