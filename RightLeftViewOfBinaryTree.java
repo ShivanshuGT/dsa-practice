5e70523a914a5f4a9cdaf00c8ffcc936016d2ed4
ShivanshuGT <shivanshumdnr@gmail.com>
2025-08-31 21:30:17 +0530

    right and left view of binary tree

---
+++ RightLeftViewOfBinaryTree.java
@@ -0,0 +1,129 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class RightLeftViewOfBinaryTree {
+
+    static class TreeNode {
+        int data;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int data){
+            this.data = data;
+        }
+        
+    }
+
+    private static TreeNode buildTreeFromInput(String[] input){
+        if(input.length == 0 || input[0].equals("null")){
+            return null;
+        }
+
+        TreeNode root = new TreeNode(Integer.parseInt(input[0]));
+        int n = input.length;
+        int i = 1;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (i<n && !queue.isEmpty()) {
+            TreeNode current = queue.poll();
+            if(i<n && !input[i].equals("null")){
+                current.left = new TreeNode(Integer.parseInt(input[i]));
+                queue.offer(current.left);
+            }
+            i += 1;
+            if(i<n && !input[i].equals("null")){
+                current.right = new TreeNode(Integer.parseInt(input[i]));
+                queue.offer(current.right);
+            }
+            i += 1;
+
+        }
+        return root;
+    }
+
+    private static void rightViewOfBinaryTree(TreeNode root, int level, List<Integer> traversal){
+        // We will be doing reverse preorder traversal here
+        // Root Right Left
+        if(root == null){
+            return;
+        }
+        if(level == traversal.size()){
+            traversal.add(root.data);
+        }
+        rightViewOfBinaryTree(root.right, level+1, traversal);
+        rightViewOfBinaryTree(root.left, level+1, traversal);
+
+        // TC -> O(n)
+        // SC -> O(H) (= O(n) in worst case when the tree is skewed)
+    }
+
+    private static void leftViewOfBinaryTree(TreeNode root, int level, List<Integer> traversal){
+        // We will be doing preorder traversal here
+        // Root Left Right
+        if(root == null){
+            return;
+        }
+        if(level == traversal.size()){
+            traversal.add(root.data);
+        }
+        leftViewOfBinaryTree(root.left, level+1, traversal);
+        leftViewOfBinaryTree(root.right, level+1, traversal);
+
+        // TC -> O(n)
+        // SC -> O(H) (= O(n) in worst case when the tree is skewed)
+    }
+
+    private static List<List<Integer>> levelOrderTraversal(TreeNode root){
+        
+        List<List<Integer>> ans = new ArrayList<>();
+        if(root == null){
+            return ans;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int times = queue.size();
+            List<Integer> list = new ArrayList<>();
+            while (times > 0) {
+                if(queue.peek().left != null){
+                    queue.offer(queue.peek().left);
+                }
+                if(queue.peek().right != null){
+                    queue.offer(queue.peek().right);
+                }
+                list.add(queue.poll().data);
+                times -= 1;
+            }
+            ans.add(list);
+        }
+
+        return ans;
+
+        // TC -> O(n) since we are touching each node only once
+        // SC -> O(n) since we are storing the nodes in a queue
+        // where n is the number of nodes in the tree
+        
+
+    
+    }
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] input = sc.nextLine().split("\\s+");
+        TreeNode root = buildTreeFromInput(input);
+        // System.out.println(levelOrderTraversal(root));
+        List<Integer> leftView = new ArrayList<>();
+        leftViewOfBinaryTree(root, 0, leftView);
+        List<Integer> rightView = new ArrayList<>();
+        rightViewOfBinaryTree(root, 0, rightView);
+        System.out.println(rightView);
+        System.out.println(leftView);
+        sc.close();
+    }
+}
