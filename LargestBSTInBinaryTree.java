8a1c1f64a3104b8263b133fb53c6b430be9fd5b3
ShivanshuGT <shivanshumdnr@gmail.com>
2025-09-23 19:49:11 +0530

    largest BST in a binary tree

---
+++ LargestBSTInBinaryTree.java
@@ -0,0 +1,126 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class LargestBSTInBinaryTree {
+
+    static class TreeNode {
+        int data;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int data){
+            this.data = data;
+        }
+        
+    }
+
+    static class MinMaxHolder{
+        int size;
+        int minValue;
+        int maxValue;
+
+        MinMaxHolder(int size, int minValue, int maxValue){
+            this.size = size;
+            this.minValue = minValue;
+            this.maxValue = maxValue;
+        }
+    }
+
+    private static TreeNode buildTreeFromInput(String[] input){
+        if(input.length == 0 || input[0].equals("null")){
+            return null;
+        }
+
+        TreeNode root = new TreeNode(Integer.parseInt(input[0]));
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int i = 1;
+        int n = input.length;
+
+        while (i<n && !queue.isEmpty()) {
+            TreeNode current = queue.poll();
+            if(i<n && !input[i].equals("null")){
+                current.left = new TreeNode(Integer.parseInt(input[i]));
+                queue.offer(current.left);
+            }
+            i += 1;
+            if(i<n && !input[i].equals("null")){
+                current.right = new TreeNode(Integer.parseInt(input[i]));
+                queue.offer(current.right);
+            }
+            i += 1;
+        }
+        return root;
+        // TC -> O(n)
+        // SC -> O(n)
+    }
+
+    private static List<List<Integer>> levelOrderTraversal(TreeNode root){
+        
+        List<List<Integer>> ans = new ArrayList<>();
+        if(root == null){
+            return ans;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int times = queue.size();
+            List<Integer> list = new ArrayList<>();
+            while (times > 0) {
+                if(queue.peek().left != null){
+                    queue.offer(queue.peek().left);
+                }
+                if(queue.peek().right != null){
+                    queue.offer(queue.peek().right);
+                }
+                list.add(queue.poll().data);
+                times -= 1;
+            }
+            ans.add(list);
+        }
+
+        return ans;
+
+        // TC -> O(n) since we are touching each node only once
+        // SC -> O(n) since we are storing the nodes in a queue
+        // where n is the number of nodes in the tree
+    }
+
+    private static MinMaxHolder findLargestBSTOptimal(TreeNode root){
+        // post order traversal - left right root
+        if(root == null){
+            return new MinMaxHolder(0, Integer.MAX_VALUE, Integer.MIN_VALUE);
+        }
+        MinMaxHolder left = findLargestBSTOptimal(root.left);
+        MinMaxHolder right = findLargestBSTOptimal(root.right);
+
+        if(left.maxValue < root.data  && root.data < right.minValue){
+            // it is a valid BST
+            int min = Math.min(right.minValue, Math.min(left.minValue, root.data));
+            int max = Math.max(right.maxValue, Math.max(root.data, left.maxValue));
+            return new MinMaxHolder(left.size + right.size + 1, min, max);
+        }else{
+            // it is not a valid BST
+            return new MinMaxHolder(Math.max(left.size, right.size), Integer.MIN_VALUE, Integer.MAX_VALUE);
+        }
+        // TC -> O(n)
+        // SC -> O(n) (memory stack space)
+
+
+    }
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] input = sc.nextLine().split("\\s+");
+        TreeNode root = buildTreeFromInput(input);
+        System.out.println(levelOrderTraversal(root));
+        MinMaxHolder result = findLargestBSTOptimal(root);
+        System.out.println(result == null ? 0 : result.size);
+        sc.close();
+    }
+}
