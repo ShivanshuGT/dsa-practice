2162a34db19be7b40e0b83c5266b535917cf3974
ShivanshuGT <shivanshumdnr@gmail.com>
2025-12-13 13:10:46 +0530

    count subsets with given difference

--- CountPartitionsWithGivenDifference.java
+++ CountPartitionsWithGivenDifference.java
@@ -1,37 +1,63 @@
 import java.util.Scanner;
 
-public class CountSubsetsWithSumK {
+public class CountPartitionsWithGivenDifference {
 
-    private static int countSubsetsRecursiveHelper(int ind, int target, int[] arr, int[][] dp){
+    /*
+    We have to divide arr into two partitions with sum s1 and s2 such that
+    s1 >= s2
+
+    k = s1 - s2
+    and we have sum = s1+s2, so s1 = sum-s2
+    k = sum -s2 -s2
+    k = sum - 2s2
+    s2 = (sum-k)/2
+    so, effectively we have to find the number of subsets that have the sum = (sum-k)/2
+    
+    */
+
+    private static int findNumberOfSubsetsRecursiveHelper(int ind, int target, int[] arr, int[][] dp){
         // base cases
         if(ind == 0){
-            if(target == 0 && arr[ind] == 0){
+            if(arr[0] == 0 && target == 0){
                 return 2;
             }
             if(target == 0){
                 return 1;
             }
-            if(target == arr[ind]){
+            if(arr[0] == target){
                 return 1;
             }
             return 0;
         }
 
-        int skipCurrent = countSubsetsRecursiveHelper(ind-1, target, arr, dp);
+        if(dp[ind][target] != -1){
+            return dp[ind][target];
+        }
+
+        int skipCurrent = findNumberOfSubsetsRecursiveHelper(ind-1, target, arr, dp);
         int pickCurrent = 0;
         if(arr[ind] <= target){
-            pickCurrent = countSubsetsRecursiveHelper(ind-1, target-arr[ind], arr, dp);
+            pickCurrent = findNumberOfSubsetsRecursiveHelper(ind-1, target-arr[ind], arr, dp);
         }
+
         dp[ind][target] = skipCurrent + pickCurrent;
         return dp[ind][target];
         // TC -> O(n x target)
         // SC -> O(n x target) + O(n)
-        
     }
-
-
-    private static int countSubsetsRecursive(int[] arr, int target){
+    private static int findNumberOfSubsetsRecursive(int[] arr, int k){
         int n = arr.length;
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];
+        }
+
+        if(((sum-k) < 0) || (((sum-k)%2) != 0)){
+            // if (sum-k) is negative or (sum-k)/2 is a fraction
+            return 0;
+        }
+
+        int target = (sum-k)/2;
         int[][] dp = new int[n][target+1];
 
         for (int i = 0; i < n; i++) {
@@ -39,16 +65,29 @@ public class CountSubsetsWithSumK {
                 dp[i][j] = -1;
             }
         }
+        return findNumberOfSubsetsRecursiveHelper(n-1, target, arr, dp);
 
-        return countSubsetsRecursiveHelper(n-1, target, arr, dp);
         // TC -> O(n x target)
         // SC -> O(n x target) + O(n)
+        // where target = (sum-k)/2
+
     }
 
-    private static int countSubsetsTabulation(int[] arr, int target){
+    private static int findNumberOfSubsetsTabulation(int[] arr, int k){
         int n = arr.length;
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];
+        }
+
+        if(((sum-k) < 0) || (((sum-k)%2) != 0)){
+            // if (sum-k) is negative or (sum-k)/2 is a fraction
+            return 0;
+        }
+
+        int target = (sum-k)/2;
         int[][] dp = new int[n][target+1];
-        
+
         // base cases
         if(arr[0] == 0){
             dp[0][0] = 2;
@@ -70,15 +109,25 @@ public class CountSubsetsWithSumK {
                 dp[i][j] = skipCurrent + pickCurrent;
             }
         }
-
         return dp[n-1][target];
+
         // TC -> O(n x target)
         // SC -> O(n x target)
     }
 
-    private static int countSubsetsSpaceOptimized(int[] arr, int target){
+    private static int findNumberOfSubsetsSpaceOptimized(int[] arr, int k){
         int n = arr.length;
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];
+        }
 
+        if(((sum-k) < 0) || (((sum-k)%2) != 0)){
+            // if (sum-k) is negative or (sum-k)/2 is a fraction
+            return 0;
+        }
+
+        int target = (sum-k)/2;
         int[] dp = new int[target+1];
 
         // base cases
@@ -88,7 +137,7 @@ public class CountSubsetsWithSumK {
             dp[0] = 1;
         }
 
-        if(arr[0] != 0 && arr[0] <= target){
+        if((arr[0] != 0) && (arr[0] <= target)){
             dp[arr[0]] = 1;
         }
 
@@ -104,23 +153,22 @@ public class CountSubsetsWithSumK {
             }
             dp = current;
         }
+
         return dp[target];
         // TC -> O(n x target)
         // SC -> O(target)
     }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int target = sc.nextInt();
+        int k = sc.nextInt();
         int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
-        System.out.println(countSubsetsRecursive(arr, target));
-        System.out.println(countSubsetsTabulation(arr, target));
-        System.out.println(countSubsetsSpaceOptimized(arr, target));
-        sc.close();
+        System.out.println(findNumberOfSubsetsRecursive(arr, k));
+        System.out.println(findNumberOfSubsetsTabulation(arr, k));
+        System.out.println(findNumberOfSubsetsSpaceOptimized(arr, k));
         sc.close();
     }
 }
